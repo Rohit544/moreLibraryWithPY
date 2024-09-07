14a2ef0c49a9d24af304124d8d69b3f759eecb69
Rohit544 <monuoo1009@gmail.com>
2024-09-07 21:39:49 +0530

    uncomment

--- another.py
+++ another.py
@@ -3,33 +3,33 @@ import numpy as np
 import pandas as pd 
 
 # Sample data
-# x = [1, 2, 3, 4, 5]
-# y = [2, 3, 5, 7, 11]
+x = [1, 2, 3, 4, 5]
+y = [2, 3, 5, 7, 11]
 
-# # Create a plot
-# plt.plot(x, y)
-
-
-# plt.plot(x, y, label="Line")
-# plt.legend()
+# Create a plot
+plt.plot(x, y)
 
 
-# # Show the plot
-# sizes = [15, 30, 45, 10]
-# labels = ['A', 'B', 'C', 'D']
-
-# plt.pie(sizes, labels=labels, autopct='%1.1f%%')
-# plt.title("Pie Chart")
-# plt.show()
+plt.plot(x, y, label="Line")
+plt.legend()
 
 
+# Show the plot
+sizes = [15, 30, 45, 10]
+labels = ['A', 'B', 'C', 'D']
 
-# data = np.random.randn(1000)  # Generate random data
-# plt.hist(data, bins=10, color='red')
-# plt.title("Histogram")
-# plt.xlabel("Value")
-# plt.ylabel("Frequency")
-# plt.show()
+plt.pie(sizes, labels=labels, autopct='%1.1f%%')
+plt.title("Pie Chart")
+plt.show()
+
+
+
+data = np.random.randn(1000)  # Generate random data
+plt.hist(data, bins=10, color='red')
+plt.title("Histogram")
+plt.xlabel("Value")
+plt.ylabel("Frequency")
+plt.show()
 
 data = {
     'Name': ['Alice', 'Bob', 'Charlie'],
